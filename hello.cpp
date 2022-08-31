366fb8b844225bdff76e5815c2bec98e2573dc54
Bismakeepslay <111972630+Bismakeepslay@users.noreply.github.com>
2022-08-31 14:10:06 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "Nama:Bisma"<< endl;
+   cout << "Kelas:X PPLG 1"<< endl;
+   cout << "______________" << endl;
+   cout << "angka1 = 10" << endl;
+   cout << "angka2 = 5" << endl;
+   cout << "hasil:" << 10+5 << endl;
+    return 0;
+    }
